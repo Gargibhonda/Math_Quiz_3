0035d68739835a2a4c238b7c7276ec6c5a308fc5
Gargibhonda <78081438+Gargibhonda@users.noreply.github.com>
2021-11-09 14:24:38 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -24,5 +24,4 @@ function send(){
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
-
 }
